50f3d4670eb8ff2c1c44485fd6dfa4246bf29194
testerJJ <wxl1989beyond@163.com>
2019-08-04 09:49:45 +0800

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("wo ca");
+		JSONObject a = new JSONObject();
+		a.put("a","123");
+		System.out.println(a.get("a"));
+
+	}
+}
